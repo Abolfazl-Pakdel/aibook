4abf5f77092cfc926d13cb54bb5dd78604ce4e6b
APP <Abolfazlpakdelpour@gmail.com>
2026-06-29 17:33:40 +0330

    adding python stats

--- stats.py
+++ stats.py
@@ -1,1 +1,15 @@
-# just a place holder
+# write a stats collector in python 
+import os
+import sys
+import time
+import datetime
+import json
+import requests
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import statsmodels.tsa.api as tsa
+import statsmodels.tsa.stattools as sts
